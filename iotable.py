58ab2c7e81a235e9c9dd731c1188a41123c36b73
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2016-01-04 11:40:23 -0200

    iotable.py prints also value

--- iotable.py
+++ iotable.py
@@ -15,21 +15,32 @@ def to_iotable(name, rounds=None):
         grouped_table = table.groupby(['seller', 'buyer'])
         quantities = grouped_table.sum()['quantity']
         prices = grouped_table.mean()['price']
+        value = quantities * prices
         quantities = quantities.unstack()
         quantities = quantities.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=1)
         quantities = quantities.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=0)
         quantities = quantities.replace(np.NaN, 0)
-        quantities['sum'] = sum([quantities[name] for name in quantities.columns])
-        quantities.ix['sum'] = sum([quantities.ix[name] for name in quantities.T.columns])
         prices = prices.unstack()
         prices = prices.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=0)
         prices = prices.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=1)
         prices = prices.replace(np.NaN, 0)
+
+        table['value'] = table['quantity'] * table['price']
+        #vtable.drop(['round', 'index'], axis=1, inplace=True)
+        vgrouped_table = table.groupby(['seller', 'buyer'])
+        values = vgrouped_table.sum()['value']
+        values = values.unstack()
+        values = values.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=1)
+        values = values.reindex_axis(['col', 'ele', 'gas', 'o_g', 'oil', 'eis', 'trn', 'roe', 'lab', 'cap', 'tax', 'household', 'inv', 'netexport'], axis=0)
+        values = values.replace(np.NaN, 0)
+        print '***\tvalues\t***'
+        print values
         print '***\tprice\t***'
         print prices
         print '***\tquantities\t***'
         print quantities
-        return quantities
+        return value
+
 
 
 
@@ -40,4 +51,4 @@ def to_iotable(name, rounds=None):
 
 
 if __name__ == '__main__':
-    quantities = to_iotable('./result/cce_2016-01-03_21-25I/')
+    value = to_iotable('./result/cce_2016-01-04_11-30')
