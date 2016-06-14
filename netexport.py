7a458e9773ff2a5290b1130e52b4e0ede2e843ed
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2016-06-14 15:12:51 -0300

    netexport keeps money for the next transaction, as a resurt the mean price = 1 value is 2664.7952095596461

--- netexport.py
+++ netexport.py
@@ -6,12 +6,18 @@ class NetExport(abce.Agent):
         self.create('money', 1000000)
 
     def international_trade(self):
+        net_income = 0
         for group in self.get_offers_all().values():
             for offer in group:
                 if offer.buysell == 98:
                     self.create(offer.good, offer.quantity)
+                    net_income += offer.quantity * offer.price
+                else:
+                    net_income -= offer.quantity * offer.price
+
                 self.accept(offer)
-        trade_surplus = self.possession('money') - 1000000
+        target = 1000000 - net_income
+        trade_surplus = self.possession('money') - target
         self.log('', {'trade_surplus': trade_surplus})
         self.take('household', 0, good='money', quantity=max(0, - trade_surplus))
         self.give('household', 0, good='money', quantity=max(0, trade_surplus))
