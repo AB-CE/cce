8741df0cd1b9f2f2dabaa412240403ad05d61cd0
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2016-06-14 18:53:26 -0300

    investment has as much money as it needs for next rounds transaction

--- investment.py
+++ investment.py
@@ -6,10 +6,13 @@ class Investment(abce.Agent):
         self.create('money', 1000000)
 
     def investment(self):
+        net_income = 0
         for group in self.get_offers_all().values():
             for offer in group:
                 self.accept(offer)
-        trade_surplus = self.possession('money') - 1000000
+                net_income -= offer.quantity * offer.price
+        target = 1000000 - net_income
+        trade_surplus = self.possession('money') - target
         self.log('', {'trade_surplus': trade_surplus})
         self.take('household', 0, good='money', quantity=max(0, - trade_surplus))
         self.log('money', self.possession('money'))
